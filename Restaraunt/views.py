149c871e2ae53e328710439644ac8aca733f3842
Stefan <stefancool00@gmail.com>
2024-04-29 20:55:32 -0700

    Auth and Tokens

--- Restaraunt/views.py
+++ Restaraunt/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 
@@ -7,6 +10,7 @@ def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
@@ -15,5 +19,11 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     serializer_class = MenuSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def msg(request):
+    return Response({"message" : "This view is protected"})
